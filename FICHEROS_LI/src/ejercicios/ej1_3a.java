1bbfce7b7f893cc901327dc293d8474bea4f2c23
alexsans23 <alexandra.candrea23@gmail.com>
2025-03-07 13:51:49 +0100

    a

--- FICHEROS_LI/src/ejercicios/ej1_3a.java
+++ FICHEROS_LI/src/ejercicios/ej1_3a.java
@@ -1,12 +1,39 @@
 package ejercicios;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ej1_3a {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		File o = new File("original.txt");
+		File c = new File("copia.txt");
 		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(o));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(c));
+			
+			String linea;
+			while((linea = br.readLine()) != null) {
+				bw.write(linea);
+				bw.newLine();
+			}					
+			br.close();
+			bw.close();
+					
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 
 	}
--- FICHEROS_LI/src/ejercicios/ej1_3b.java
+++ FICHEROS_LI/src/ejercicios/ej1_3b.java
@@ -1,9 +1,41 @@
 package ejercicios;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ej1_3b {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("introduce el nombre del fichero que deseas copiar");
+		String ruta = sc.nextLine();
+		File f = new File(ruta);
+		File c = new File("copia_de_"+ ruta);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(c));
+			
+			String linea;
+			while((linea = br.readLine()) != null) {
+				bw.write(linea);
+				bw.newLine();
+			}					
+			br.close();
+			bw.close();
+					
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 
 	}
 
